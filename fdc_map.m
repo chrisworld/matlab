0b572231bf56fc7d3f30c60372b04f8c9bb4af77
Christian Walter <christian.walter@student.tugraz.at>
2018-02-04 18:54:47 +0100

    ml pe runs

--- fdc_map.m
+++ fdc_map.m
@@ -3,65 +3,82 @@
 % Christian Walter, 01431717
 %---------------------
 
+%clc
+%close all
+clear all
+
+% packages for octave
+pkg load communications		% for qfunc
+
 % params
 % ---
 % realizations
-N = 10^(2);
-
-% snr
-snr = -4 : 0.5 : 12;
-snr_lin = 10.^(snr/10);
-
-% noise vector
-n = randn(3, N);
-% check normal distribution
-mean(n);
-var(n);
-figure 1;
-hist(n, 100);
+N = 10^(5);
 
 % signals params
 e_av = 1;
 Pm1 = 0.05;
 Pm2 = 0.35;
 Pm3 = 0.60;
-Nm1 = Pm1 * N;
-Nm2 = Pm2 * N;
-Nm3 = Pm3 * N;
 % create the signal vectors
-sm1 = [e_av*ones(1, Nm1); zeros(2, Nm1)];
-sm2 = [zeros(1, Nm2); e_av*ones(1, Nm2); zeros(1,Nm2)];
-sm3 = [zeros(2, Nm3); e_av*ones(1, Nm3)];
+sm1 = e_av*[1; 0; 0];
+sm2 = e_av*[0; 1; 0];
+sm3 = e_av*[0; 0; 1];
+sm123 = [sm1, sm2, sm3];
+% samples of the signal signal
+sm = [repmat(sm1, 1, Pm1*N), repmat(sm2, 1, Pm2*N), repmat(sm3, 1, Pm3*N)];
 % permut the signal vectors
-sm = [sm1, sm2, sm3];
 rsm = randperm(N);
 sm = sm(:, rsm);
 
-% signal space model
-r = sm + n;
-size(r)
+% labels
+labels = [repmat(1, 1, Pm1*N), repmat(2, 1, Pm2*N), repmat(3, 1, Pm3*N)];
+labels = labels(:, rsm);
 
-for k = 1 : 1 : N
-  sm(:, k)
+% loop the power
+for snr = -4 : 1 : 12;
+  snr_lin = 10.^(snr/10);
+  % Power spectral density of noise
+  N0 = e_av./snr_lin;
+  % noise vector
+  % Power spectral density of noise
+  n = N0 .* randn(3, N);
+  % check normal distribution
+  %mean_val = mean(n(1,:))
+  %variance = var(n(1,:))
+  %figure 1;
+  %hist(n(1,:), 100);
+
+  % signal space model
+  r = sm + n;
+
+  % min distance algorithm
+  err_count = 0;
+  for sample = 1 : 1 : N
+    [val m] = min(sum((repmat(r(:, sample), 1, 3) - sm123).^2, 1));
+    if m == labels(sample);
+    else err_count++;
+    end
+  end
+  Pe_ml(end + 1) = err_count / N;
 end
-
+Pe_ml
 
 % calc. approx. error probability
 % nearest neighbors Ne
 Ne = 2;
-Papx = Ne * qfunc(sqrt(snr_lin));
 
-% plot
+snr = -4 : 1 : 12;
+snr_lin = 10.^(snr/10);
+Pe_apx = Ne * qfunc(sqrt(snr_lin));
+
 figure 2;
-plot(snr, 10*log(Papx));
-grid on;
-title('Error probabilities');
-
-figure 3;
-semilogy(snr, Papx);
+semilogy(snr, Pe_apx, 'k--');
+hold on;
+semilogy(snr, Pe_ml, 'r');
 grid on;
 title('Error Probabilities P_e');
 xlabel('e_{av}/N_0 / [dB]');
 ylabel('P_e');
-legend('approx. P_e')
+legend('approx. P_e', 'ML P_e')
 
