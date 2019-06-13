867efcea41c1e13d5d522a1853529ddecec632a2
Walter <chrisworld3@tugraz.at>
2019-06-13 17:12:28 +0200

    [nibci] e6 wrong scores

--- nibci_e6/e6_csp.m
+++ nibci_e6/e6_csp.m
@@ -1,7 +1,7 @@
 
-%close all;
-%clear all;
-%clc;
+close all;
+clear all;
+clc;
 
 % load data
 load('Sig.mat')
@@ -9,8 +9,15 @@ load('ClassPosH.mat')
 load('ClassPosF.mat')
 load('SampleRate.mat')
 
+% --
+% CSP
+
 % common data
 n_channels = size(Sig, 1);
+
+% classes
+class_hand = 1;
+class_feet = 2;
 
 % band pass filtering
 filter_order = 4;
@@ -28,42 +35,84 @@ end
 % choose interesting time segments in samples
 samples_cue = [1.5, 2.5] * SR;
 
-% classes
-class_hand = 1;
-class_feet = 2;
-
 % data matrix
-X = zeros(100, 128, n_channels);
+X = zeros(100, n_channels, 128);
 
-% for all channels
+% epoch channels
 for ch = 1 : n_channels
-
-  % data matrix channel
-  X_ch = [];
-
   % create cues for class hand
   for trig = 1 : length(ClassPosH)
-    vec = s_band(ch, ClassPosH(trig)) + samples_cue(1) : s_band(ch, ClassPosH(trig)) + samples_cue(2) - 1;
-    X_ch = [X_ch; vec];
+    X(trig, ch, :) = s_band(ch, ClassPosH(trig)) + samples_cue(1) : s_band(ch, ClassPosH(trig)) + samples_cue(2) - 1;
   end
 
   % create cues for class feet
   for trig = 1 : length(ClassPosF)
-    vec = s_band(ch, ClassPosF(trig)) + samples_cue(1) : s_band(ch, ClassPosF(trig)) + samples_cue(2) - 1;
-    X_ch = [X_ch; vec];
+    X(trig + 50, ch, :) = s_band(ch, ClassPosF(trig)) + samples_cue(1) : s_band(ch, ClassPosF(trig)) + samples_cue(2) - 1;
   end
-  
-  % add channel to data matrix
-  X(:, :, ch) = X_ch;
 end
 
-size(X)
-class_vec = [ones(1, length(ClassPosH)) * class_hand, ones(1, length(ClassPosF)) * class_feet];
+% calculate separate spatial normalized covariance
+sigm = zeros(100, 15, 15);
+for trial = 1 : 100
+  XX_t = squeeze(X(trial, :, :)) * transpose(squeeze(X(trial, :, :)));
+  sigm(trial, :, :) = XX_t / trace(XX_t);
+end
 
-% CSP
+% average covariance matrices
+sigm1 = squeeze(mean(sigm(1:50, :, :), 1));
+sigm2 = squeeze(mean(sigm(51:100, :, :), 1));
+
+% calculate eigenvalues
+[V, D] = eig(sigm1, sigm1 + sigm2, 'qz');
+
+% sort eigenvalues according to magnitude in D
+[D_s, D_si] = sort(D * ones(15, 1));
+V_s = V(:, D_si);
+
+
+% --
+% extract CSP and train LDA
+
+% use most important filters
+C = [V_s(:, 1), V_s(:, 2), V_s(:, 14), V_s(:, 15)];
+
+% CSP filtering
+s_csp = C' * s_band;
+
+% data matrix CSP
+X_csp = zeros(100, 4, 128);
+
+% epoch channels with csp filtering
+for ch = 1 : 4
+  % create cues for class hand
+  for trig = 1 : length(ClassPosH)
+    X_csp(trig, ch, :) = s_csp(ch, ClassPosH(trig)) + samples_cue(1) : s_csp(ch, ClassPosH(trig)) + samples_cue(2) - 1;
+  end
+
+  % create cues for class feet
+  for trig = 1 : length(ClassPosF)
+    X_csp(trig + 50, ch, :) = s_csp(ch, ClassPosF(trig)) + samples_cue(1) : s_csp(ch, ClassPosF(trig)) + samples_cue(2) - 1;
+  end
+end
+
+% calculate band power
+x_train = log10(sum(X_csp .^ 2, 3));
+
+% get labels
+y_train = [ones(1, length(ClassPosH)) * class_hand, ones(1, length(ClassPosF)) * class_feet];
+
+% training
+[w, b] = custom_LDA(x_train, y_train);
+
+% compute scores on train and test set
+score_train = custom_score(sign(w' * x_train' - b), y_train)
+
+
+% --
+% 
 
 % Laplacian to compare
-
+% accuracy 85%
 
 
 
